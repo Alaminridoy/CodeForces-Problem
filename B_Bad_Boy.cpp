bd311dace74d732d58f67c6a95dd241d18578190
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2025-10-23 14:33:50 +0600

    cp-sheet 31

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define bismillah()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long
+#define nl '\n'
+void sol()
+{
+    int n , m , i ,j;
+    cin >> n >> m >> i>> j;
+
+    cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+
+
+}
+signed main()
+{
+    bismillah();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
+}
