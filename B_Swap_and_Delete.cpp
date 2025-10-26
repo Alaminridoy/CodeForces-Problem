f4e246921d6f1710e963072d73459cdea8acf415
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2025-10-26 22:09:47 +0600

    cp-sheet 31

--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -14,46 +14,34 @@ void sol()
     int zero = count(s.begin(), s.end(), '0');
     int one = count(s.begin(), s.end(), '1');
 
-    if (zero == 1 || one == 1)
-    {
-        cout << 1 << nl;
-        return;
-    }
-    else if (zero == one)
-    {
-        cout << 0 << nl;
-        return;
-    }
-
     int cnt = 0;
     for (int i = 0; i < s.size(); i++)
     {
-        if (zero == 0 || one == 0)
-        {
-            cout << s.size() - cnt << nl;
-            break;
-        }
-
+        
         if (s[i] == '1')
         {
             zero--;
-            cnt++;
         }
-        else
-        {
+        else if(s[i] == '0'){
             one--;
-            cnt++;
         }
-    }
-}
-    signed main()
-    {
-        bismillah();
-        int t = 1;
-        cin >> t;
-        while (t--)
+
+        if (zero < 0 || one < 0)
         {
-            sol();
+            cout << s.size() - i << nl;
+            return;
         }
-        return 0;
     }
+    cout << 0 << endl;
+}
+signed main()
+{
+    bismillah();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
+}
